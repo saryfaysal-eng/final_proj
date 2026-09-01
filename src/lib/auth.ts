419daa2a92bbd6c159be0dea8b67d6291b85fdfa
saryfaysal-eng <sary.faysal@isae.edu.lb>
2026-09-01 11:52:12 +0300

    proj setup NextJS, Prisma 7, NeonDB, BetterAuth

---
+++ src/lib/auth.ts
@@ -0,0 +1,10 @@
+import { PrismaClient } from "@prisma/client";
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+
+const client = new PrismaClient();
+
+export const auth = betterAuth({
+  database: prismaAdapter(client, { provider: "postgresql" }),
+  baseURL: "http://localhost:3000/",
+});
