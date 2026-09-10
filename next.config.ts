e3a666a92f48e0904ac6887f48df0d54f972d18a
IbrahimHamdeh24 <IbrahimHamadeh24@gmail.com>
2026-09-10 21:00:11 +0300

    increase next.js max delay for http requests to reduce timeout errors

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
+import dns from "node:dns";
+import { Agent, setGlobalDispatcher } from "undici";
 
+dns.setDefaultResultOrder("ipv4first");
+
+setGlobalDispatcher(
+  new Agent({
+    connectTimeout: 60_000,
+    headersTimeout: 60_000,
+    bodyTimeout: 60_000,
+  }),
+);
 const nextConfig: NextConfig = {
-  /* config options here */
   devIndicators: false,
   images: {
     remotePatterns: [
